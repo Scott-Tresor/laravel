e188fc865c5c5d2b6cec534306a14bdf108dd5b4
scott <scotttresor@gmail.com>
2019-09-13 15:20:45 +0200

    ajout du barre de navigation projetlaravel

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -13,6 +13,47 @@
 
     </head>
     <body>
+        <header class="header-area">
+            <div class="main-header-area">
+                <div class="classy-nav-container breakpoint-off">
+                    <div class="container">
+                        <nav class="classy-navbar justify-content-between" id="alimeNav">
+                            <!-- Logo -->
+                            <a class="nav-brand mt-3" href="/">Navbar</a>
+                            <div class="classy-navbar-toggler">
+                                <span class="navbarToggler">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </span>
+                            </div>
+                            <div class="classy-menu">
+                                <div class="classycloseIcon">
+                                    <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                                </div>
+                                <div class="classynav">
+                                    <ul class="nav justify-content-end">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="/">Accueil</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="./stock.html">Stocks</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="./about.html">Apropos</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="./contact.html">Contact</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+
         <div class="container mt-5">
             @include('flash::message')
         </div>
