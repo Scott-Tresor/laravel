339924fb358663f970f9b6b90f2574af240f1669
scott <scotttresor@gmail.com>
2019-09-14 15:51:26 +0200

    simplification de la barre de navigation et les autres fonctionnalites

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,5 +1,22 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    @auth
+        <div class="container">
+            <div class="col-lg-4 ml-auto mt-2">
+                <div class="text-right">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item mr-0" >
+                            <a
+                                class="nav-link {{ request()->is(auth()->user()->email) ? 'active' : '' }}"
+                                href="/{{ auth()->user()->email }}"
+                                style="text-transform: uppercase; text-decoration: none;">
+                            {{ auth()->user()->username }}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    @endauth
+    <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
             <a class="navbar-brand {{ request()->is('/') ? 'active' : '' }}" href="/">
                 <h3>FiveLab</h3>
@@ -10,21 +27,14 @@
             </button>
             <div class="collapse navbar-collapse ml-lg-5" id="navbarNav">
                 <ul class="navbar-nav ml-lg-auto">
-                    @if(auth()->check())
-                        <li class="nav-item ">
-                            <a class="nav-link {{ request()->is('mon-compte') ? 'active' : '' }}" href="/mon-compte">Mon-compte</a>
-                        </li>
-                        <li class="nav-item d-md-inline-block pl-lg-3 pr-0">
-                            <a class="nav-link {{ request()->is('deconnexion') ? 'active' : '' }}" href="/deconnexion">Deconnexion</a>
-                        </li>
+                    @auth
+                        @include('layout.navbar-item', ['lien' => '/', 'nom' => 'Contact'])
+                        @include('layout.navbar-item', ['lien' => 'mon-compte', 'nom' => 'mon-compte'])
+                        @include('layout.navbar-item', ['lien' => 'deconnexion', 'nom' => 'deconnexion'])
                     @else
-                        <li class="nav-item d-md-inline-block pl-lg-3 pr-0">
-                            <a class="nav-link {{ request()->is('connexion') ? 'active' : '' }}" href="/connexion">Connexion</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link {{ request()->is('inscription') ? 'active' : '' }}" href="/inscription">Inscription</a>
-                        </li>
-                    @endif
+                        @include('layout.navbar-item', ['lien' => 'connexion', 'nom' => 'connexion'])
+                        @include('layout.navbar-item', ['lien' => 'inscription', 'nom' => 'inscription'])auth
+                    @endauth
                 </ul>
             </div>
         </div>
